2dbd4582a2be9a97c5063642ecb28134e1e6112c
thepegasus <thepegasus@users.noreply.github.com>
2014-05-04 15:05:48 +0530

    Basic controls in place

--- pegabrowser.py
+++ pegabrowser.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
 from tkinter import *
-
+#from tkinter.ttk import *
 class pegaBrowser(Frame):
+    url=None
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
-        self.hi=Button(self, text="Launch")
-        self.hi.pack()
+        self.initializeScreen()
+    def initializeScreen(self):
+        self.addressLabel=Label(text="Address")
+        self.addressLabel.pack(side=LEFT)
+        self.addressBar=Entry(width=100)
+        #For easy debug
+        self.addressBar.insert(0,"localhost")
+        self.addressBar.pack(side=LEFT)
+        self.goButton=Button(text="Go")
+        self.goButton.pack(side=LEFT)
+        self.goButton.bind("<Button-1>",self.goButtonHandler)
+        self.contentArea=Label(text="Content Area")
+        self.contentArea.pack(side=BOTTOM, fill=X, expand=1)
+    def goButtonHandler(self, event):
+        self.url=self.addressBar.get()
+        self.contentArea["text"]="Fetching "+self.url
+            
 
 root=Tk()
-root.wm_title("PegaBrowser - 0.1")
-root.minsize(600,400)
+root.title("PegaBrowser - 0.1")
+#root.minsize(600,400)
+#Start the window maximized
+#w,h=root.winfo_screenwidth(), root.winfo_screenheight()
+#root.geometry("%dx%d+0+0" % (w,h)) 
 pegabrowser=pegaBrowser(root)
-pegabrowser.master.title=("PegaBrowser - 0.1")
 pegabrowser.mainloop()
-#print("Completed")
