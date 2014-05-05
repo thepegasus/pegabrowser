9612c2fbecb149c3110181c480c55c84c2d66016
thepegasus <thepegasus@users.noreply.github.com>
2014-05-05 17:44:34 +0530

    Fetched data from given URL

--- pegabrowser.py
+++ pegabrowser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from tkinter import *
+from urllib.request import *
 #from tkinter.ttk import *
 class pegaBrowser(Frame):
     url=None
@@ -12,20 +13,30 @@ class pegaBrowser(Frame):
         self.addressLabel.pack(side=LEFT)
         self.addressBar=Entry(width=100)
         #For easy debug
-        self.addressBar.insert(0,"localhost")
+        self.addressBar.insert(0,"http://google.com")
         self.addressBar.pack(side=LEFT)
         self.goButton=Button(text="Go")
         self.goButton.pack(side=LEFT)
         self.goButton.bind("<Button-1>",self.goButtonHandler)
         self.contentArea=Label(text="Content Area")
-        self.contentArea.pack(side=BOTTOM, fill=X, expand=1)
+        self.contentArea.pack(side=LEFT, fill=X, expand=1)
     def goButtonHandler(self, event):
         self.url=self.addressBar.get()
-        self.contentArea["text"]="Fetching "+self.url
-            
+        #self.contentArea["text"]="Fetching "+self.url
+        htmlData=getHttpResource(self.url)    
+        self.contentArea["text"]=htmlData
+
+def getHttpResource(url):
+        #Get resource from server
+        response=urlopen(url)
+        html=response.read()
+        return html
+
+       
 
 root=Tk()
 root.title("PegaBrowser - 0.1")
+
 #root.minsize(600,400)
 #Start the window maximized
 #w,h=root.winfo_screenwidth(), root.winfo_screenheight()
